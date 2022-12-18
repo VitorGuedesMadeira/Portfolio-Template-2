d6174d8f7a91eb83a4e00356421f03f9b1f75c4a
VitorGuedesMadeira <vgm_rox@hotmail.com>
2022-12-18 13:05:33 -0300

    Add styles

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -13,7 +13,7 @@ const Contact = () => {
               <span>Get In Touch</span>
             </div>
             <p>If you want to work together on a project or just have a chat, please don't hesitate to contact me via email below.</p>
-            <a class="email-link" href="mailto:vgm_rox@hotmail.com" target="_blank" loquacious-marshmallow-469259
+            <a class="email-link" href="mailto:vgm_rox@hotmail.com" target="_blank" rel="noreferrer"
 >Say Hello</a>
           </div>
           <div className="relative">
@@ -25,7 +25,7 @@ const Contact = () => {
           <span><small>© All rights reserved</small></span>
           <ul>
             <li><a href="https://instagram.com/vitorguedesmadeira" target="_blank" rel="noreferrer"><FaInstagram /></a></li>
-            <li><a href="https://github.com/VitorGuedesMadeira" target="_blank" loquacious-marshmallow-469259
+            <li><a href="https://github.com/VitorGuedesMadeira" target="_blank" rel="noreferrer"
 ><FaGithub /></a></li>
             <li><a href="https://www.linkedin.com/in/vitor-guedes-madeira/" target="_blank" rel="noreferrer"><FaLinkedin /></a></li>
           </ul>
