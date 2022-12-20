4d7ffc68b23e16f82ef6891775b3f5a0846a543d
VitorGuedesMadeira <vgm_rox@hotmail.com>
2022-12-20 15:57:53 -0300

    Add style to skills

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -1,4 +1,13 @@
 import { FaAngleRight, FaLocationArrow } from 'react-icons/fa';
+import icon1 from '../../assets/icons/javascript.png'
+import icon2 from '../../assets/icons/react.png'
+import icon3 from '../../assets/icons/ruby.png'
+import icon4 from '../../assets/icons/bootstrap.png'
+import icon5 from '../../assets/icons/tailwind.png'
+import icon6 from '../../assets/icons/node.png'
+import icon7 from '../../assets/icons/photoshop.png'
+import icon8 from '../../assets/icons/postgresql.png'
+import icon9 from '../../assets/icons/vscode.png'
 import './Skills.css';
 
 const Skills = () => {
@@ -13,40 +22,40 @@ const Skills = () => {
           <div className="skills-box">
             <ul>
               <li>
-                <FaAngleRight className="small-arrow-icon" />
-                <span>JavaScript</span>
+                <img className="skill-icon" src={icon1} alt="skill-icon" />
+                <p className="skill-name">JavaScript</p>
               </li>
               <li>
-                <FaAngleRight className="small-arrow-icon" />
-                <span>VSCode</span>
+                <img className="skill-icon" src={icon2} alt="skill-icon" />
+                <p className="skill-name">React</p>
               </li>
               <li>
-                <FaAngleRight className="small-arrow-icon" />
-                <span>RSpec</span>
+                <img className="skill-icon" src={icon3} alt="skill-icon" />
+                <p className="skill-name">Ruby</p>
               </li>
               <li>
-                <FaAngleRight className="small-arrow-icon" />
-                <span>React/Redux</span>
+                <img className="skill-icon" src={icon4} alt="skill-icon" />
+                <p className="skill-name">Bootstrap</p>
               </li>
               <li>
-                <FaAngleRight className="small-arrow-icon" />
-                <span>Node.js</span>
+                <img className="skill-icon" src={icon5} alt="skill-icon" />
+                <p className="skill-name">Tailwind</p>
               </li>
               <li>
-                <FaAngleRight className="small-arrow-icon" />
-                <span>Photoshop</span>
+                <img className="skill-icon" src={icon6} alt="skill-icon" />
+                <p className="skill-name">Node JS</p>
               </li>
               <li>
-                <FaAngleRight className="small-arrow-icon" />
-                <span>Ruby/Rails</span>
+                <img className="skill-icon" src={icon7} alt="skill-icon" />
+                <p className="skill-name">Photoshop</p>
               </li>
               <li>
-                <FaAngleRight className="small-arrow-icon" />
-                <span>PostgreSQL</span>
+                <img className="skill-icon" src={icon8} alt="skill-icon" />
+                <p className="skill-name">PosgreSQL</p>
               </li>
               <li>
-                <FaAngleRight className="small-arrow-icon" />
-                <span>Capybara</span>
+                <img className="skill-icon" src={icon9} alt="skill-icon" />
+                <p className="skill-name">VSCode</p>
               </li>
             </ul>
           </div>
