7a9a3f8aeb5d73eb76b60e198d095f73dea6aee9
VitorGuedesMadeira <vgm_rox@hotmail.com>
2022-12-20 11:43:22 -0300

    Add styles to skills section

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -10,44 +10,46 @@ const Skills = () => {
             <span>Skills</span>
             <p className="microverse-date">Those are my stacks</p>
           </div>
-          <ul>
-            <li>
-              <FaAngleRight className="small-arrow-icon" />
-              <span>JavaScript</span>
-            </li>
-            <li>
-              <FaAngleRight className="small-arrow-icon" />
-              <span>VSCode</span>
-            </li>
-            <li>
-              <FaAngleRight className="small-arrow-icon" />
-              <span>RSpec</span>
-            </li>
-            <li>
-              <FaAngleRight className="small-arrow-icon" />
-              <span>React/Redux</span>
-            </li>
-            <li>
-              <FaAngleRight className="small-arrow-icon" />
-              <span>Node.js</span>
-            </li>
-            <li>
-              <FaAngleRight className="small-arrow-icon" />
-              <span>Photoshop</span>
-            </li>
-            <li>
-              <FaAngleRight className="small-arrow-icon" />
-              <span>Ruby/Rails</span>
-            </li>
-            <li>
-              <FaAngleRight className="small-arrow-icon" />
-              <span>PostgreSQL</span>
-            </li>
-            <li>
-              <FaAngleRight className="small-arrow-icon" />
-              <span>Capybara</span>
-            </li>
-          </ul>
+          <div className="skills-box">
+            <ul>
+              <li>
+                <FaAngleRight className="small-arrow-icon" />
+                <span>JavaScript</span>
+              </li>
+              <li>
+                <FaAngleRight className="small-arrow-icon" />
+                <span>VSCode</span>
+              </li>
+              <li>
+                <FaAngleRight className="small-arrow-icon" />
+                <span>RSpec</span>
+              </li>
+              <li>
+                <FaAngleRight className="small-arrow-icon" />
+                <span>React/Redux</span>
+              </li>
+              <li>
+                <FaAngleRight className="small-arrow-icon" />
+                <span>Node.js</span>
+              </li>
+              <li>
+                <FaAngleRight className="small-arrow-icon" />
+                <span>Photoshop</span>
+              </li>
+              <li>
+                <FaAngleRight className="small-arrow-icon" />
+                <span>Ruby/Rails</span>
+              </li>
+              <li>
+                <FaAngleRight className="small-arrow-icon" />
+                <span>PostgreSQL</span>
+              </li>
+              <li>
+                <FaAngleRight className="small-arrow-icon" />
+                <span>Capybara</span>
+              </li>
+            </ul>
+          </div>
         </div>
       </section>
     );
